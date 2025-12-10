fea198b47b7343f46b8d60ac2ff7996108dbdf83
Kai Hughes <kaijohnhughes2004@gmail.com>
2025-12-10 16:08:29 +1100

    nvm

--- frontend/src/src/services/loginAuthenticate.jsx
+++ frontend/src/src/services/loginAuthenticate.jsx
@@ -4,7 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3030
 
 export const login = async (email, password) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}v1/users/login`, {
+    const response = await axios.post(`${API_BASE_URL}/v1/users/login`, {
       email,
       password,
     });
